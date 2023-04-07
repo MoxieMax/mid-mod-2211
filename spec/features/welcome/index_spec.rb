12ab4416eaf89e7fe51017c04170321fdde41dc5
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-07 08:32:43 -0700

    Created initial welcome index spec file, page loads but won't search yet.

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "/", type: :feature do
+  
+  before :each do
+    visit "/"
+  end
+  
+  describe "as a user when I visit welcome#index" do
+    it "has a form" do
+      expect(current_path).to eq("/")
+      expect(page).to have_field("Search")
+      expect(page).to have_button("Search")
+    end
+    
+    describe "can search for an ingredient" do
+      it "" do
+        fill_in("Search", with: "sweet potatoes")
+        
+        click_button "Search"
+        
+        expect(current_path).to eq("/foods")
+      end
+      
+    end
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items returned by the search.
+# (sweet potatoes should find more than 30,000 results)
+# Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"
