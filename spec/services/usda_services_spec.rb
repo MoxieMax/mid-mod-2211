dd3f02e96cefe5c5d6ac78478bff11f0912c4460
Max MacGillivray <mary.macgillivray@gmail.com>
2023-04-07 09:16:41 -0700

    Added the service testing for the search

--- spec/services/usda_services_spec.rb
+++ spec/services/usda_services_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe UsdaService do
+  describe "returns a JSON object containing search results" do
+    let(:search) {UsdaService.new.keyword_search("sweet potatoes")}
+    
+    it "exists" do
+      expect(search).to be_a(Hash)
+      expect(search.keys).to eq([
+                                :totalHits, 
+                                :currentPage, 
+                                :totalPages, 
+                                :pageList, 
+                                :foodSearchCriteria, 
+                                :foods, 
+                                :aggregations
+                                ])
+      expect(search[:totalHits]).to be > 30000
+      expect(search[:totalHits]).to eq(51270)
+      # binding.pry
+    end
+    
+  end
 end
